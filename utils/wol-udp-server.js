05670fab210e91d7cfa1f035ca621c6cfe7db64c
roccomuso <rocco@hackerstribe.com>
2016-12-16 22:06:56 +0000

    util UDP server for packet analysis

---
+++ utils/wol-udp-server.js
@@ -0,0 +1,19 @@
+// Sample UDP server to debug wol packets
+
+var PORT = 9;
+var HOST = '0.0.0.0';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + JSON.stringify(message));
+});
+
+server.bind(PORT, HOST);
+
